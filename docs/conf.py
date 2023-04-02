ec5facaebbfcf877ed1912d19ca2352ad98ec62e
samukele-dev <samukele107@gmail.com>
2023-04-02 22:45:16 +0200

    docs_setting_up

--- docs/conf.py
+++ docs/conf.py
@@ -8,8 +8,11 @@
 
 import os
 import sys
+import django
 
 sys.path.insert(0, os.path.abspath('..'))
+os.enviro['DJANGO_SETTINGS_MODULE'] = 'Schools_Academy'
+django.setup()
 
 project = 'schools'
 copyright = '2023, Samukele'
