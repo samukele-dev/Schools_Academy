18795ec083b2dd8e739dc05fbd283ca4413a404f
samukele-dev <samukele107@gmail.com>
2023-07-12 10:09:59 +0200

    deploying on render

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,9 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolio_db_wzx2',
+        'USER': 'samukele',
+        'PASSWORD': 'QRh3PQmPIWO0v87YrOOpKr78cPhOiuwM',
+        'HOST': 'dpg-chhkpmrhp8ufj5upvpb0-a.oregon-postgres.render.com',
     }
 }
 
